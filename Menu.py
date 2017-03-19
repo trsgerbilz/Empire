3488ba5f4f41c534993c6f54caf7908afc5d0f04
trsgerbilz <trsgerbilz@gmail.com>
2017-03-19 23:57:45 +1300

    V0.01 There's a menu with 3 buttons

--- Menu.py
+++ Menu.py
@@ -7,12 +7,32 @@ window_size = window_width, window_height = 800, 600
 window_centre = window_width // 2, window_height // 2
 window = pygame.display.set_mode(window_size)
 
-menu_state = 'main'  # main, play, editor, settings
-
 black = 0, 0, 0
 white = 255, 255, 255
 
+state = 'main_menu'
+
 window_clock = pygame.time.Clock()
+
+
+class State:
+    def __init__(self):
+        global state
+        self.state = state
+        MainMenu().render()
+
+    def main_menu(self):
+        MainMenu().render()
+        self.state = 'main_menu'
+
+        """
+        elif state == 'play':
+            pass  # todo play screen
+        elif state == 'editor':
+            pass  # todo editor screen
+        elif state == 'settings':
+            pass  # todo settings screen
+        """
 
 
 class MainMenu:
@@ -21,9 +41,9 @@ class MainMenu:
         self.editor_box = self.draw_box(-65, 45, 130, 50)
         self.settings_box = self.draw_box(-80, 115, 160, 50)
 
-    def draw_text(self, text, size, height=0):
+    def draw_text(self, content, size, height=0):
         font = pygame.font.SysFont('', size)
-        text = font.render(text, True, black)
+        text = font.render(content, True, black)
         text_centre = text.get_width() // 2, text.get_height() // 2 + height
         text_pos = np.subtract(window_centre, text_centre)
         window.blit(text, text_pos)
@@ -33,7 +53,7 @@ class MainMenu:
         pygame.draw.rect(window, black, box_rect, 7)
         return box_rect
 
-    def main_menu_render(self):
+    def render(self):
         window.fill(white)
         self.draw_text("Empire", 72, 100)  # ==Title==
         self.play_box = self.draw_box(-50, -25, 100, 50)  # ==Play Box==
@@ -53,8 +73,36 @@ class MainMenu:
             print("Settings")
 
 
+class Editor:
+    def draw_text(self, content, size, height=0):
+        font = pygame.font.SysFont('', size)
+        text = font.render(content, True, black)
+        text_centre = text.get_width() // 2, text.get_height() // 2 + height
+        text_pos = np.subtract(window_centre, text_centre)
+        window.blit(text, text_pos)
+
+    def draw_box(self, xpos, ypos, xsize, ysize):
+        box_rect = pygame.Rect(window_centre[0] + xpos, window_centre[1] + ypos, xsize, ysize)
+        pygame.draw.rect(window, black, box_rect, 7)
+        return box_rect
+
+    def toolbar(self):
+        pygame.image.load("Icons/add.png")
+
+
+class LeftMouseEvents:
+    def __init__(self):
+        global state
+        self.state = state
+
+        if state == 'main_menu':
+            MainMenu().button_events(pygame.mouse.get_pos())
+
+
 def main():
     """main"""
+
+    State()
 
     while 1:
         for event in pygame.event.get():
@@ -62,16 +110,7 @@ def main():
                 pygame.quit()
                 quit()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                MainMenu().button_events(pygame.mouse.get_pos())  # todo this needs a better handler
-
-        if menu_state == 'main':
-            MainMenu().main_menu_render()
-        elif menu_state == 'play':
-            pass  # todo play screen
-        elif menu_state == 'editor':
-            pass  # todo editor screen
-        elif menu_state == 'settings':
-            pass  # todo settings screen
+                LeftMouseEvents()
 
         pygame.display.update()
         window_clock.tick(60)
